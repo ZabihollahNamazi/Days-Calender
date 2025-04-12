4020e5bd967091b90eec872285e0b8fdc83082bc
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-04-12 22:16:43 +0100

    updated the function to show all days of a month in the table

--- web.mjs
+++ web.mjs
@@ -11,23 +11,25 @@ let fistDayOfWeek = new Date(year, month, 1).getDay();
 fistDayOfWeek = fistDayOfWeek == 0 ? 6 : fistDayOfWeek == 6 ? 0 : fistDayOfWeek - 1;
 console.log(fistDayOfWeek)
 console.log(year, " ", month)
+console.log("total days, " , new Date(year, month + 2, 0).getDate())
 function displayCalender(year, month){
     let fistDayOfWeek = new Date(year, month, 1).getDay(); // gets first day of week (0-6), in computer starts from sunday = 0
     fistDayOfWeek = fistDayOfWeek == 0 ? 6 : fistDayOfWeek == 6 ? 0 : fistDayOfWeek - 1; // formatted to start from monday = 0
     let dayCount = 1;
+    const totalDaysInMonth = new Date(year, month + 1, 0).getDate(); // 0 gives you the last day of the previous month, but we add month by 1 to get the total day of current month
     
     let table = document.querySelector("#table-calender");
 
     let firstRowOfCalender = document.createElement("tr");
-    for(let i = 0; i < fistDayOfWeek; i++){
+    for(let i = 0; i < fistDayOfWeek; i++){  //this loop makes cells for empty days before starting first day of first week or row
         let emptyDay = document.createElement("th");
         emptyDay.innerHTML = "-";
         emptyDay.style.border = "solid black";
         firstRowOfCalender.appendChild(emptyDay)
     }
-    table.append(firstRowOfCalender)
+    table.appendChild(firstRowOfCalender)
 
-    for(let i = fistDayOfWeek; i < 7; i++){
+    for(let i = fistDayOfWeek; i < 7; i++){ //this loop make cells from first day for first week or first row
         let firstRowDay = document.createElement("th");
         firstRowDay.innerHTML = dayCount;
         dayCount++;
@@ -35,8 +37,23 @@ function displayCalender(year, month){
         firstRowOfCalender.appendChild(firstRowDay)
     }
     table.appendChild(firstRowOfCalender)
+
+    while(dayCount <= totalDaysInMonth){
+        let newRow = document.createElement("tr");
+        for(let j = 0; j <= 6; j++){
+            if (dayCount > totalDaysInMonth) break;
+            let restDay = document.createElement("th");
+            restDay.innerHTML = dayCount;
+            restDay.style.border = "solid black";
+            newRow.appendChild(restDay);
+            dayCount++;
+        }
+        table.appendChild(newRow)
+        
+
+    }
 }
 
 window.onload = function() {
-    displayCalender(2025, 4)
+    displayCalender(2025, 3)
 }
