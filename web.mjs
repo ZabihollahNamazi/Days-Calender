1cb89d648496f07f0db7a31289cfc74766161572
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-04-12 21:50:00 +0100

    generated the first row of table

--- web.mjs
+++ web.mjs
@@ -5,7 +5,38 @@
 
 import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
+let year = new Date().getFullYear();
+let month = new Date().getMonth();
+let fistDayOfWeek = new Date(year, month, 1).getDay();
+fistDayOfWeek = fistDayOfWeek == 0 ? 6 : fistDayOfWeek == 6 ? 0 : fistDayOfWeek - 1;
+console.log(fistDayOfWeek)
+console.log(year, " ", month)
+function displayCalender(year, month){
+    let fistDayOfWeek = new Date(year, month, 1).getDay(); // gets first day of week (0-6), in computer starts from sunday = 0
+    fistDayOfWeek = fistDayOfWeek == 0 ? 6 : fistDayOfWeek == 6 ? 0 : fistDayOfWeek - 1; // formatted to start from monday = 0
+    let dayCount = 1;
+    
+    let table = document.querySelector("#table-calender");
+
+    let firstRowOfCalender = document.createElement("tr");
+    for(let i = 0; i < fistDayOfWeek; i++){
+        let emptyDay = document.createElement("th");
+        emptyDay.innerHTML = "-";
+        emptyDay.style.border = "solid black";
+        firstRowOfCalender.appendChild(emptyDay)
+    }
+    table.append(firstRowOfCalender)
+
+    for(let i = fistDayOfWeek; i < 7; i++){
+        let firstRowDay = document.createElement("th");
+        firstRowDay.innerHTML = dayCount;
+        dayCount++;
+        firstRowDay.style.border = "solid black";
+        firstRowOfCalender.appendChild(firstRowDay)
+    }
+    table.appendChild(firstRowOfCalender)
+}
 
 window.onload = function() {
-    document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
+    displayCalender(2025, 4)
 }
