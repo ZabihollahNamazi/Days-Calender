cc59708a5ccab1eb547e6247ea3bf80687e0ab11
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-04-13 22:21:31 +0100

    upadated the prenext function to show 1th 2nd or more pre or next month, it was showing only one pre or one next month

--- web.mjs
+++ web.mjs
@@ -6,13 +6,27 @@
 import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
+let year = new Date().getFullYear();
+let month = new Date().getMonth();
+
 function displayCalender(year, month){
     let fistDayOfWeek = new Date(year, month, 1).getDay(); // gets first day of week (0-6), in computer starts from sunday = 0
-    fistDayOfWeek = fistDayOfWeek == 0 ? 6 : fistDayOfWeek == 6 ? 0 : fistDayOfWeek - 1; // formatted to start from monday = 0
+    fistDayOfWeek = fistDayOfWeek == 0 ? 6 : fistDayOfWeek - 1; // formatted to start from monday = 0
     let dayCount = 1;
     const totalDaysInMonth = new Date(year, month + 1, 0).getDate(); // 0 gives you the last day of the previous month, but we add month by 1 to get the total day of current month
-    
-    let table = document.querySelector("#table-calender");
+    console.log(year, " , ", month)
+    let table = document.getElementById("table-calender");
+    table.innerHTML = ""; // This clears the previous calendar
+
+    const weekDaysName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let weekDaysNameRow = document.createElement("tr");
+    for(let wd = 0; wd <= 6; wd++){ //this loop make cells from first day for first week or first row
+        let weekDay = document.createElement("th");
+        weekDay.innerHTML = weekDaysName[wd];
+        weekDay.style.border = "solid black";
+        weekDaysNameRow.appendChild(weekDay)
+    }
+    table.appendChild(weekDaysNameRow)
 
     let firstRowOfCalender = document.createElement("tr");
     for(let i = 0; i < fistDayOfWeek; i++){  //this loop makes cells for empty days before starting first day of first week or row
@@ -46,33 +60,30 @@ function displayCalender(year, month){
     }
 }
 
-function preNextBtn(year, month){
+function preNextBtn(){
     let previousBtn = document.getElementById("previous");
     let next = document.getElementById("next");
 
     previousBtn.addEventListener("click", ()=>{
-        let preMonth = month - 1;
-        let preYear = year;
+        month--;
         if(month < 0){
-            preMonth = 11;
+            month = 11;
             preYear--;
         }
-        displayCalender(preYear, preMonth)
+        displayCalender(year, month)
     })
 
     next.addEventListener("click", ()=>{
-        let nextMonth = month + 1;
-        let nextYear = year;
+        month++;
         if(month > 11){
-            nextMonth = 0;
+            month = 0;
             nextYear++
         }
-        displayCalender(nextYear, nextMonth)
+        displayCalender(year, month)
     })
 }
 window.onload = function() {
-    let year = new Date().getFullYear();
-    let month = new Date().getMonth();
+    
     displayCalender(year, month)
-    preNextBtn(year, month)
+    preNextBtn()
 }
